c530cfea0526e8cb44b53c39349444c79d97f178
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2023-05-30 10:21:34 -0300

    Base React project.

---
+++ src/component/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+	return <>Oi</>;
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import {defineConfig} from 'vite';
+import {resolve} from 'path';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: [{
+			find: '@',
+			replacement: resolve(__dirname, 'src'),
+		}],
+	},
+	base: '/string-tension-calc',
+	server: {
+		port: 8080,
+	},
+	build: {
+		outDir: 'build',
+	},
+});
