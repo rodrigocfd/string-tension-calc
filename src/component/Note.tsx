a27d4200517ce72bea3ae870d23161c978aeb54d
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2023-06-04 11:20:07 -0300

    Note change.

--- src/component/Note.tsx
+++ src/component/Note.tsx
@@ -1,14 +1,25 @@
+import {useMemo} from 'react';
 import {INote} from '@/model/types';
+import * as c from '@/model/consts';
 
 interface Props {
 	strIndex: number;
 	note: INote;
+	onChange(note: INote): void;
 }
 
 function Note(props: Props) {
-	return <>
+	const pitches = useMemo(
+		() => c.PITCHES_FOR_STRING.find(p => p.stringIndex === props.strIndex)!.pitches,
+	[props.strIndex]);
 
-	</>;
+	return <select value={props.note} onChange={e => props.onChange(e.target.value as INote)}>
+		{pitches.map(pitch =>
+			<option key={pitch.note} value={pitch.note}>
+				{pitch.note} {pitch.descr}
+			</option>
+		)}
+	</select>;
 }
 
 export default Note;
--- src/component/StringRow.tsx
+++ src/component/StringRow.tsx
@@ -4,6 +4,7 @@ import {IGuitar, IString} from '@/model/types';
 import {useStore} from '@/model/store';
 import * as c from '@/model/consts';
 import Gauge from './Gauge';
+import Note from './Note';
 
 interface Props {
 	strIndex: number;
@@ -14,18 +15,28 @@ interface Props {
 function StringRow(props: Props) {
 	const unit = useStore(s => s.unit);
 	const changeGauge = useStore(s => s.changeGauge);
+	const changeNote = useStore(s => s.changeNote);
 
 	const isModifGauge = useMemo(() => {
 		const pack = c.PACKS.find(p => p.name === props.guitar.packName)!;
 		return pack.gauges[props.strIndex] !== props.str.gauge;
 	}, [props.guitar.packName, props.strIndex, props.str.gauge]);
 
+	const isModifNote = useMemo(() => {
+		const tuning = c.TUNINGS.find(t => t.name === props.guitar.tuningName)!;
+		return tuning.notes[props.strIndex] !== props.str.note;
+	}, [props.guitar.tuningName, props.strIndex, props.str.note]);
+
 	return <DivStringRow>
 		<DivStrName>{c.STRING_NAMES[props.strIndex]}</DivStrName>
 		<DivModif modif={isModifGauge}>
 			<Gauge gauge={props.str.gauge} onChange={g => changeGauge(props.guitar, props.str, g)} />
 		</DivModif>
-
+		<DivModif modif={isModifNote}>
+			<Note strIndex={props.strIndex}
+				note={props.str.note}
+				onChange={n => changeNote(props.guitar, props.str, n)} />
+		</DivModif>
 		<div>
 			<InputTension type='text' value={props.str.tension.toFixed(2)} disabled /> {unit}
 		</div>
