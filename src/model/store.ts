fba2f2b40e3b429c87892cd94bb34fda16df388b
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2023-06-07 23:36:06 -0300

    Immer middleware.

--- src/model/store.ts
+++ src/model/store.ts
@@ -1,113 +1,98 @@
 import {create} from 'zustand';
 import {combine} from 'zustand/middleware';
+import {immer} from 'zustand/middleware/immer';
 import {IGauge, IGuitar, INote, IPackName, IScale, IString, ITuningName, IUnit} from './types';
 import {calcTension} from './funcs';
 import * as c from './consts';
 
-export const useStore = create(
+export const useStore = create(immer(
 	combine({
 		unit: 'kg/cm' as IUnit,
 		guitars: [] as IGuitar[],
 	},
 	(set, get) => ({
 		setUnit(unit: IUnit): void {
-			set(state => ({
-				unit,
-				guitars: state.guitars.map(g => ({
-					...g,
-					strings: g.strings.map((s, strIdx) => ({
-						...s,
-						tension: calcTension(strIdx, g.strings.length,
-							s.gauge, s.note, g.scale, unit),
-					})),
-				})),
-			}));
+			set(state => {
+				state.unit = unit;
+				state.guitars.forEach(guitar => {
+					guitar.strings.forEach((str, strIdx) => {
+						str.tension = calcTension(strIdx, guitar.strings.length,
+							str.gauge, str.note, guitar.scale, state.unit);
+					});
+				});
+			});
 		},
 
 		addNew(): void {
-			set(state => ({
-				guitars: [...state.guitars, {
+			set(state => {
+				state.guitars.push({
 					_key: genKey(),
 					scale: c.DEFAULT_SCALE,
 					packName: c.DEFAULT_PACK_NAME,
 					tuningName: c.DEFAULT_TUNING_NAME,
 					strings: genStrings(c.DEFAULT_PACK_NAME, c.DEFAULT_TUNING_NAME, c.DEFAULT_SCALE, state.unit),
-				}],
-			}));
+				});
+			});
 		},
 		moveLeft(guitar: IGuitar): void {
 			set(state => {
-				const guitars = [...state.guitars];
-				const idx = guitars.findIndex(g => g._key === guitar._key)!;
-				const tmp = guitars[idx];
-				guitars[idx] = guitars[idx - 1];
-				guitars[idx - 1] = tmp;
-				return {guitars};
+				const idx = state.guitars.findIndex(g => g._key === guitar._key)!;
+				const tmp = state.guitars[idx];
+				state.guitars[idx] = state.guitars[idx - 1];
+				state.guitars[idx - 1] = tmp;
 			});
 		},
 		remove(guitar: IGuitar): void {
-			set(state => ({
-				guitars: state.guitars.filter(g => g._key !== guitar._key),
-			}));
+			set(state => {
+				state.guitars = state.guitars.filter(g => g._key !== guitar._key);
+			});
 		},
 		changeScale(guitar: IGuitar, scale: IScale): void {
 			set(state => {
-				const guitars = [...state.guitars];
-				const ourGtr = guitars.find(g => g._key === guitar._key)!;
+				const ourGtr = state.guitars.find(g => g._key === guitar._key)!;
 				ourGtr.scale = scale;
 				ourGtr.strings.forEach((str, strIdx) =>
 					str.tension = calcTension(strIdx, ourGtr.strings.length,
 						str.gauge, str.note, scale, state.unit),
 				);
-				return {guitars};
 			});
 		},
 		changePack(guitar: IGuitar, name: IPackName): void {
 			set(state => {
-				const guitars = [...state.guitars];
-				const ourGtr = guitars.find(g => g._key === guitar._key)!;
+				const ourGtr = state.guitars.find(g => g._key === guitar._key)!;
 				ourGtr.packName = name;
 				ourGtr.strings = genStrings(name, ourGtr.tuningName, ourGtr.scale, state.unit);
-				return {guitars};
 			});
 		},
 		changeTuning(guitar: IGuitar, name: ITuningName): void {
 			set(state => {
-				const guitars = [...state.guitars];
-				const ourGtr = guitars.find(g => g._key === guitar._key)!;
+				const ourGtr = state.guitars.find(g => g._key === guitar._key)!;
 				ourGtr.tuningName = name;
 				ourGtr.strings = genStrings(ourGtr.packName, name, ourGtr.scale, state.unit);
-				return {guitars};
 			});
 		},
 		changeGauge(guitar: IGuitar, str: IString, gauge: IGauge): void {
 			set(state => {
-				const guitars = [...state.guitars];
-				const ourGtr = guitars.find(g => g._key === guitar._key)!;
-				const strings = [...ourGtr.strings];
-				const strIdx = strings.findIndex(s => s._key === str._key);
-				const ourStr = strings[strIdx];
+				const ourGtr = state.guitars.find(g => g._key === guitar._key)!;
+				const strIdx = ourGtr.strings.findIndex(s => s._key === str._key);
+				const ourStr = ourGtr.strings[strIdx];
 				ourStr.gauge = gauge;
 				ourStr.tension = calcTension(strIdx, guitar.strings.length,
 					gauge, ourStr.note, guitar.scale, state.unit);
-				return {guitars};
 			});
 		},
 		changeNote(guitar: IGuitar, str: IString, note: INote): void {
 			set(state => {
-				const guitars = [...state.guitars];
-				const ourGtr = guitars.find(g => g._key === guitar._key)!;
-				const strings = [...ourGtr.strings];
-				const strIdx = strings.findIndex(s => s._key === str._key);
-				const ourStr = strings[strIdx];
+				const ourGtr = state.guitars.find(g => g._key === guitar._key)!;
+				const strIdx = ourGtr.strings.findIndex(s => s._key === str._key);
+				const ourStr = ourGtr.strings[strIdx];
 				ourStr.note = note;
 				ourStr.tension = calcTension(strIdx, guitar.strings.length,
 					str.gauge, note, guitar.scale, state.unit);
-				return {guitars};
 			});
 		},
 	})),
-);
+));
 
 let curKey = 0;
 function genKey(): number {
