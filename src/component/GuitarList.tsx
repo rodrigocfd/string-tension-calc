d239718f70d13614c15f359c301547796aefb5fe
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2023-11-08 18:19:32 -0300

    Uninstall React Spring.

--- src/component/GuitarList.tsx
+++ src/component/GuitarList.tsx
@@ -1,24 +1,15 @@
-import {animated, useTransition} from '@react-spring/web';
 import {useStore} from '@/model/store';
 import Guitar from './Guitar';
 import s from '@/component-styles/GuitarList.module.scss';
 
 export default function GuitarList() {
 	const guitars = useStore(s => s.guitars);
-	const trans = useTransition(guitars, {
-		from: {opacity: 0, transform: 'translateY(-100px)'},
-		enter: {opacity: 1, transform: 'translateY(0)'},
-		leave: {opacity: 0, transform: 'translateY(-100px)'},
-		config: {
-			duration: 200,
-		},
-	});
 
-	return <div>
-		{trans((style, g, state, gtrIdx) =>
-			<animated.div className={s.gtrBlock} style={style}>
-				<Guitar guitarIndex={gtrIdx} guitar={g} />
-			</animated.div>
+	return <>
+		{guitars.map((g, idx) =>
+			<div key={g._id} className={s.gtrBlock}>
+				<Guitar guitarIndex={idx} guitar={g} />
+			</div>
 		)}
-	</div>;
+	</>;
 }
