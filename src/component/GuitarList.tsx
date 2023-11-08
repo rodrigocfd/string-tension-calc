5fc6bea4384c1471b629f1cbcd3f2df5e1c9faaa
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2023-11-08 18:48:33 -0300

    Animated insert/remove with Framer Motion.

--- src/component/GuitarList.tsx
+++ src/component/GuitarList.tsx
@@ -1,15 +1,22 @@
+import {AnimatePresence, motion} from 'framer-motion';
 import {useStore} from '@/model/store';
 import Guitar from './Guitar';
 import s from '@/component-styles/GuitarList.module.scss';
 
 export default function GuitarList() {
 	const guitars = useStore(s => s.guitars);
+	const animation = {
+		initial: {scale: 0},
+		animate: {scale: 1},
+		exit: {scale: 0},
+		transition: {duration: .25},
+	};
 
-	return <>
+	return <AnimatePresence>
 		{guitars.map((g, idx) =>
-			<div key={g._id} className={s.gtrBlock}>
+			<motion.div key={g._id} className={s.gtrBlock} layout {...animation}>
 				<Guitar guitarIndex={idx} guitar={g} />
-			</div>
+			</motion.div>
 		)}
-	</>;
+	</AnimatePresence>;
 }
