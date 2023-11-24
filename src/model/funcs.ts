3561360321fa01baa2e155cf11f1e030e5c49d61
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2023-11-23 21:54:40 -0300

    Store refactoring.

--- src/model/funcs.ts
+++ src/model/funcs.ts
@@ -1,9 +1,55 @@
-import {IGauge, INote, IScale, IUnit} from './types';
+import {IGauge, INote, IPackName, IScale, IString, ITuningName, IUnit} from './types';
 import * as c from './consts';
 
+let curKey = 0;
+export function genKey(): number {
+	return curKey++;
+}
+
+export function genStrings(
+	packName: IPackName,
+	tuningName: ITuningName,
+	scale: IScale,
+	unit: IUnit,
+): IString[] {
+	const pack = c.PACKS.find(pack => pack.name === packName)!;
+	return pack.gauges.map((gauge, strIdx) => {
+		const tuning = c.TUNINGS.find(tuning => tuning.name === tuningName)!;
+		return {
+			_key: genKey(),
+			gauge,
+			note: tuning.notes[strIdx],
+			tension: calcTension(strIdx, pack.gauges.length,
+				gauge, tuning.notes[strIdx], scale, unit),
+		};
+	});
+}
+
+export function calcTension(
+	stringIndex: number,
+	numStrings: number,
+	gauge: IGauge,
+	note: INote,
+	scale: IScale,
+	unit: IUnit,
+): number {
+	const gaugeStr = gauge;
+	const gaugeFloat = parseFloat(gaugeStr.slice(0, -2));
+	const isPlain = gaugeStr.endsWith('P');
+	const freq = c.PITCHES.find(p => p.note === note)!.freq;
+	const effScaleLen = effectiveScaleLength(stringIndex, numStrings, scale);
+
+	let tension = polynomialGauge(gaugeFloat, isPlain) * Math.pow(2 * effScaleLen * freq, 2) / 386.4;
+
+	const unitInfo = c.UNITS.find(u => u.name === unit)!;
+	return tension * unitInfo.lbFactor;
+}
+
 function effectiveScaleLength(
-	stringIndex: number, numStrings: number, scale: IScale): number
-{
+	stringIndex: number,
+	numStrings: number,
+	scale: IScale,
+): number {
 	const diff = scale.lengthLo - scale.lengthHi;
 	const incr = diff / (numStrings - 1);
 	return scale.lengthHi + incr * stringIndex;
@@ -18,19 +64,3 @@ function polynomialGauge(gauge: number, isPlain: boolean): number {
 
 	return coefs.reduce((tot, coef, i) => tot + coef * Math.pow(gauge, i), 0);
 }
-
-export function calcTension(
-	stringIndex: number, numStrings: number, gauge: IGauge, note: INote,
-	scale: IScale, unit: IUnit): number
-{
-	const gaugeStr = gauge;
-	const gaugeFloat = parseFloat(gaugeStr.slice(0, -2));
-	const isPlain = gaugeStr.endsWith('P');
-	const freq = c.PITCHES.find(p => p.note === note)!.freq;
-	const effScaleLen = effectiveScaleLength(stringIndex, numStrings, scale);
-
-	let tension = polynomialGauge(gaugeFloat, isPlain) * Math.pow(2 * effScaleLen * freq, 2) / 386.4;
-
-	const unitInfo = c.UNITS.find(u => u.name === unit)!;
-	return tension * unitInfo.lbFactor;
-};
--- src/model/useStore.ts
+++ src/model/useStore.ts
@@ -1,103 +1,77 @@
-import {computed, reactive} from 'vue';
 import {defineStore} from 'pinia';
 import {IGauge, IGuitar, INote, IPackName, IScale, IString, ITuningName, IUnit} from './types';
-import {calcTension} from './funcs';
+import {calcTension, genKey, genStrings} from './funcs';
 import * as c from './consts';
 
-const useStore = defineStore('global', () => {
-	const state = reactive({
+const useStore = defineStore('string-tension-calc', {
+	state: () => ({
 		unit: 'kg/cm' as IUnit,
 		guitars: [] as IGuitar[],
-	});
-
-	const vals = {
-		unit: computed(() => state.unit),
-		guitars: computed(() => state.guitars),
-	};
-
-	const pub = {
+	}),
+	actions: {
 		changeUnit(u: IUnit): void {
-			state.unit = u;
-			state.guitars.forEach(g => {
+			this.unit = u;
+			this.guitars.forEach(g => {
 				const pack = c.PACKS.find(pack => pack.name === g.packName)!;
 				const tuning = c.TUNINGS.find(tuning => tuning.name === g.tuningName)!;
 				g.strings.forEach((s, strIdx) => {
 					s.tension = calcTension(strIdx, pack.gauges.length,
-						s.gauge, tuning.notes[strIdx], g.scale, state.unit);
+						s.gauge, tuning.notes[strIdx], g.scale, this.unit);
 				});
 			});
 		},
 		addNew(): void {
-			state.guitars.push({
+			this.guitars.push({
 				_key: genKey(),
 				scale: c.DEFAULT_SCALE,
 				packName: c.DEFAULT_PACK_NAME,
 				tuningName: c.DEFAULT_TUNING_NAME,
-				strings: genStrings(c.DEFAULT_PACK_NAME, c.DEFAULT_TUNING_NAME, c.DEFAULT_SCALE),
+				strings: genStrings(c.DEFAULT_PACK_NAME, c.DEFAULT_TUNING_NAME,
+					c.DEFAULT_SCALE, this.unit),
 			});
 		},
 		moveLeft(guitar: IGuitar): void {
-			const idx = state.guitars.findIndex(g => g._key === guitar._key);
-			const tmp = state.guitars[idx];
-			state.guitars[idx] = state.guitars[idx - 1];
-			state.guitars[idx - 1] = tmp;
+			const idx = this.guitars.findIndex(g => g._key === guitar._key);
+			const tmp = this.guitars[idx];
+			this.guitars[idx] = this.guitars[idx - 1];
+			this.guitars[idx - 1] = tmp;
 		},
 		remove(guitar: IGuitar): void {
-			const idx = state.guitars.findIndex(g => g._key === guitar._key);
-			state.guitars.splice(idx, 1);
+			const idx = this.guitars.findIndex(g => g._key === guitar._key);
+			this.guitars.splice(idx, 1);
 		},
 		changeScale(guitar: IGuitar, scale: IScale): void {
 			guitar.scale = scale;
 			guitar.strings.forEach((str, strIdx) =>
 				str.tension = calcTension(strIdx, guitar.strings.length,
-					str.gauge, str.note, guitar.scale, state.unit),
+					str.gauge, str.note, guitar.scale, this.unit),
 			);
 		},
 		changePack(guitar: IGuitar, name: IPackName): void {
 			guitar.packName = name;
-			guitar.strings = genStrings(guitar.packName, guitar.tuningName, guitar.scale);
+			guitar.strings = genStrings(guitar.packName, guitar.tuningName,
+				guitar.scale, this.unit);
 		},
 		changeTuning(guitar: IGuitar, name: ITuningName): void {
 			guitar.tuningName = name;
-			guitar.strings = genStrings(guitar.packName, guitar.tuningName, guitar.scale);
+			guitar.strings = genStrings(guitar.packName, guitar.tuningName,
+				guitar.scale, this.unit);
 		},
 		changeGauge(guitar: IGuitar, str: IString, gauge: IGauge): void {
 			const strIdx = guitar.strings.findIndex(s => s._key === str._key);
 			const strObj = guitar.strings[strIdx];
 			strObj.gauge = gauge;
 			strObj.tension = calcTension(strIdx, guitar.strings.length,
-				gauge, strObj.note, guitar.scale, state.unit);
+				gauge, strObj.note, guitar.scale, this.unit);
 		},
 		changeNote(guitar: IGuitar, str: IString, note: INote): void {
 			const strIdx = guitar.strings.findIndex(s => s._key === str._key);
 			const strObj = guitar.strings[strIdx];
 			strObj.note = note;
 			strObj.tension = calcTension(strIdx, guitar.strings.length,
-				strObj.gauge, note, guitar.scale, state.unit);
+				strObj.gauge, note, guitar.scale, this.unit);
 		},
-	};
-
-	return {...vals, ...pub};
+	},
 });
 
 export default useStore;
-
-let curKey = 0;
-function genKey(): number {
-	return curKey++;
-}
-
-function genStrings(packName: IPackName, tuningName: ITuningName, scale: IScale): IString[] {
-	const store = useStore();
-	const pack = c.PACKS.find(pack => pack.name === packName)!;
-	return pack.gauges.map((gauge, strIdx) => {
-		const tuning = c.TUNINGS.find(tuning => tuning.name === tuningName)!;
-		return {
-			_key: genKey(),
-			gauge,
-			note: tuning.notes[strIdx],
-			tension: calcTension(strIdx, pack.gauges.length,
-				gauge, tuning.notes[strIdx], scale, store.unit),
-		};
-	});
-}
