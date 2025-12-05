d2d6d175d5801bcdd46a691f1e750b0e6c7b689f
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2025-12-05 15:37:50 -0300

    Framer Motion.

--- src/component/GuitarList.tsx
+++ src/component/GuitarList.tsx
@@ -1,14 +1,22 @@
+import {AnimatePresence, HTMLMotionProps, motion} from 'framer-motion';
 import useStore from '~/model/useStore';
 import Guitar from './Guitar';
+
+const animation: HTMLMotionProps<'div'> = {
+	initial: {scale: 0},
+	animate: {scale: 1},
+	exit: {scale: 0},
+	transition: {duration: .25},
+};
 
 export default function GuitarList() {
 	const guitars = useStore(s => s.guitars);
 
-	return <>
+	return <AnimatePresence>
 		{guitars.map((g, idx) =>
-			<div key={g._id} className='GuitarList-block'>
+			<motion.div key={g._id} layout {...animation} className='GuitarList-block'>
 				<Guitar guitarIndex={idx} guitar={g} />
-			</div>,
+			</motion.div>,
 		)}
-	</>;
+	</AnimatePresence>;
 }
